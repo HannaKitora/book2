642c1283869276d813f9f3d5a29d699c8a2eb27e
HannaKitora <h.wimper@gmail.com>
2024-05-25 01:33:14 +0000

    view_file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
+  get 'users/index'
+  resources :books, only: [:new, :create, :index, :show]
+  
+  # get 'books/new'
+  # get 'books/index'
+  # get 'books/show'
+  
   devise_for :users
   root to: 'homes#top'
+  get '/homes/about' => 'homes#about', as: 'about'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
