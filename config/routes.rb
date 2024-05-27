b94a59f16e058ec1b1c8ed119ae0fefcf9cf99f1
HannaKitora <h.wimper@gmail.com>
2024-05-27 12:17:45 +0000

    routingOK

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  # get 'users/:id/show' => 'users#show'
-  # get 'users/:id/edit' => 'users#edit'
-  # get 'users/index' => 'users#index'
   resources :books, only: [:new, :create, :index, :show, :destroy, :edit]
   resources :users, only: [:show, :index, :edit, :update]
+  root to: 'homes#top'
+  get 'homes/about' => 'homes#about', as: 'about'
   
   # get 'books/new'
   # get 'books/index'
   # get 'books/show'
-  
-
-  root to: 'homes#top'
-  get '/homes/about' => 'homes#about', as: 'about'
-  
+  # get 'users/:id/show' => 'users#show'
+  # get 'users/:id/edit' => 'users#edit'
+  # get 'users/index' => 'users#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
